ae041c99ae473e184d06cedcc9988fe018a5bf3d
erelea00 <erelea00@estudiantes.unileon.es>
2014-06-23 20:40:18 +0200

    Created Project - Created Main class

--- LibraryStAXParser/src/es/unileon/librarystaxparser/Main.java
+++ LibraryStAXParser/src/es/unileon/librarystaxparser/Main.java
@@ -12,11 +12,8 @@ package es.unileon.librarystaxparser;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
