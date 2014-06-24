b4fda6e0c7e9b20c6d0f5c4af214646b098585b5
erelea00 <erelea00@estudiantes.unileon.es>
2014-06-24 16:37:02 +0200

    Moved checkParameters method from main to ProcessParameters

--- LibraryStAXParser/src/es/unileon/librarystaxparser/main/Main.java
+++ LibraryStAXParser/src/es/unileon/librarystaxparser/main/Main.java
@@ -2,6 +2,8 @@ package es.unileon.librarystaxparser.main;
 
 import es.unileon.librarystaxparser.exceptions.FileNotFoundException;
 import es.unileon.librarystaxparser.exceptions.NoParametersException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Adrian Diez
@@ -15,15 +17,19 @@ import es.unileon.librarystaxparser.exceptions.NoParametersException;
 public class Main {
 
     public static void main(String[] args){ 
-        
-        ParameterProcessor processor = new ParameterProcessor(args);
-        
         try {
-            processor.checkParameters();
-        } catch (NoParametersException ex) {
-            ex.printStackTrace(); //imprimir mensaje en vez de stack en la entrega final
+            ParameterProcessor processor = new ParameterProcessor(args);
+            
+            try {
+                processor.checkParameters();
+            } catch (NoParametersException ex) {
+                ex.printStackTrace(); //imprimir mensaje en vez de stack en la entrega final
+            }
+            
+        String fileName = processor.getFileName();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
         }
-               
     }
     
 }
